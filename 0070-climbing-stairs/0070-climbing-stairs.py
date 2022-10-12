c8c0f59d421c73cb5144c463f174a276ea134226
Ameya Dalvi <50326522+AmeyaDalvi@users.noreply.github.com>
2022-10-11 21:54:37 -0400

    Time: 67 ms (7.74%), Space: 13.9 MB (11.76%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,26 +1,42 @@
 class Solution:
+    # fibonacci
+    
     # def climbStairs(self, n: int) -> int:
-    #     one, two = 1, 1                 # fibonacci
+    #     one, two = 1, 1                 
     #     for i in range(n-1):
     #         temp = one
     #         one = one + two
     #         two = temp
     #     return one 
     
-    def climb_stairs(self, i, n, memo):
-        if i > n:
-            return 0
-        if i == n:
-            return 1
-        if memo[i] > 0:
-            return memo[i]
-        memo[i] = self.climb_stairs(i+1, n, memo) + self.climb_stairs(i+2, n, memo)
+    # recursion with memoization
+    
+#     def climb_stairs(self, i, n, memo):
+#         if i > n:
+#             return 0
+#         if i == n:
+#             return 1
+#         if memo[i] > 0:
+#             return memo[i]
+#         memo[i] = self.climb_stairs(i+1, n, memo) + self.climb_stairs(i+2, n, memo)
         
-        return memo[i]
+#         return memo[i]
+    
+#     def climbStairs(self, n: int) -> int:
+#         memo = [0]*n
+#         return self.climb_stairs(0, n, memo)
     
     def climbStairs(self, n: int) -> int:
-        memo = [0]*n
-        return self.climb_stairs(0, n, memo)
-    
-
+        dp = [0]*(n+1)
+        
+        if n == 1:
+            return 1
+        
+        dp[1] = 1
+        dp[2] = 2
+        
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[n]
         
