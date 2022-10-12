c85befbfdaab9c35b4f9691995e1c911837399b6
Ameya Dalvi <50326522+AmeyaDalvi@users.noreply.github.com>
2022-10-11 23:28:29 -0400

    Time: 171 ms (80.93%), Space: 16.8 MB (19.25%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,6 +1,8 @@
-class Solution:
+class Solution: 
+    # hash-map solution
+        
     # def singleNumber(self, nums: List[int]) -> int:
-    #     # hash-map solution
+    #    
     #     nums_dict={}
     #     for num in nums:
     #         if num not in nums_dict:
@@ -9,14 +11,18 @@ class Solution:
     #             nums_dict[num] += 1
     #     return [i for i in nums_dict if nums_dict[i] == 1][0]
     
-#      def singleNumber(self, nums: List[int]) -> int:  
-#           # math solution
-#           # formula = 2 * (a + b + c) - (a + a + b + b + c) = c
-            
-#           return 2*sum(set(nums)) - sum(nums)
-          
     
+     # math solution
+        
+    # def singleNumber(self, nums: List[int]) -> int:  
+    #          
+    #      # formula = 2 * (a + b + c) - (a + a + b + b + c) = c
+    #
+    #     return 2*sum(set(nums)) - sum(nums)
+          
+    # XOR solution
       def singleNumber(self, nums: List[int]) -> int:
+            
             res = 0
             for num in nums:
                 res ^= num
