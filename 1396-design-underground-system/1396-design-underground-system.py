610ed079b7cc863f2902f13f6fc00944942dc867
Ameya Dalvi <50326522+AmeyaDalvi@users.noreply.github.com>
2022-11-02 12:33:07 -0400

    Time: 571 ms (45.01%), Space: 24.2 MB (54.09%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.py
+++ 1396-design-underground-system/1396-design-underground-system.py
@@ -1,5 +1,9 @@
 from collections import defaultdict
 
+
+#use defaultdict for stations dictionary, and dict for customers. 
+# e.g customers: {45: [['Leyton', 'Waterloo'], 3]} stations: {('Leyton', 'Waterloo'): [12, 10]}
+
 class UndergroundSystem:
 
     def __init__(self):
@@ -11,23 +15,13 @@ class UndergroundSystem:
         if id not in self.customer:
             self.customer[id] = [[stationName, ""], t]
         
-        # print("customers", self.customer)
-        # print("stations", self.stations)
-        
     def checkOut(self, id: int, stationName: str, t: int) -> None:
-        if id not in self.customer:
-            return False
-        else:
-            # print(self.customer[id][0])
+        if id in self.customer:
             self.customer[id][0][1] = stationName
-            # print(self.customer[id][0])
             self.customer[id][1] = t - self.customer[id][1]
-            # print(self.customer[id])
             self.stations[(self.customer[id][0][0], self.customer[id][0][1])].append(self.customer[id][1])
         
         del self.customer[id]
-        # print("customers", self.customer)
-        # print("stations", self.stations)
         
 
     def getAverageTime(self, startStation: str, endStation: str) -> float:
