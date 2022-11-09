9901ee8e6dc01c29e097ba1bd32c5c919daec8aa
Ameya Dalvi <50326522+AmeyaDalvi@users.noreply.github.com>
2022-11-09 16:16:23 -0500

    Time: 1208 ms (30.72%), Space: 61.7 MB (15.13%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -1,50 +1,40 @@
-## Use set to store elements and use random.choices with weights and k=1
-
-
-import random
-class RandomizedSet(object):
-
+from random import choice
+class RandomizedSet():
     def __init__(self):
-        self.random_set = set()
-        
+        """
+        Initialize your data structure here.
+        """
+        self.dict = {}
+        self.list = []
 
-    def insert(self, val):
+        
+    def insert(self, val: int) -> bool:
         """
-        :type val: int
-        :rtype: bool
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
         """
-        if val in self.random_set:
+        if val in self.dict:
             return False
-        else:
-            self.random_set.add(val)
+        self.dict[val] = len(self.list)
+        self.list.append(val)
+        return True
+        
+
+    def remove(self, val: int) -> bool:
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        """
+        if val in self.dict:
+            # move the last element to the place idx of the element to delete
+            last_element, idx = self.list[-1], self.dict[val]
+            self.list[idx], self.dict[last_element] = last_element, idx
+            # delete the last element
+            self.list.pop()
+            del self.dict[val]
             return True
-        
+        return False
 
-    def remove(self, val):
+    def getRandom(self) -> int:
         """
-        :type val: int
-        :rtype: bool
+        Get a random element from the set.
         """
-        if val in self.random_set:
-            self.random_set.remove(val)
-            return True
-        else:
-            return False
-        
-        
-    def getRandom(self):
-        """
-        :rtype: int
-        """
-        p = 1//len(self.random_set)
-        if p == 0:
-            return random.choices(list(self.random_set), k=1)[0]
-        
-        return random.choices(list(self.random_set), weights = [p]*len(self.random_set), k=1)[0]
-
-
-# Your RandomizedSet object will be instantiated and called as such:
-# obj = RandomizedSet()
-# param_1 = obj.insert(val)
-# param_2 = obj.remove(val)
-# param_3 = obj.getRandom()
+        return choice(self.list)
