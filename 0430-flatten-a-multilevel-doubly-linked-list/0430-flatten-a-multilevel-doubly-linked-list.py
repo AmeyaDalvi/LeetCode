7ea6dd77f1f9a236300b9e3c293b2a33fab47a21
Ameya Dalvi <50326522+AmeyaDalvi@users.noreply.github.com>
2022-11-09 13:52:04 -0500

    Time: 26 ms (94.29%), Space: 13.9 MB (85.52%) - LeetHub

---
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.py
@@ -0,0 +1,51 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, prev, next, child):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        self.child = child
+"""
+
+
+## Use a stack to store the pointer to the node that has a child node.
+
+class Solution(object):
+    def flatten(self, head):
+        """
+        :type head: Node
+        :rtype: Node
+        """
+        curr = head
+        stack = []
+        
+        while curr:
+            if curr.child:
+                if curr.next:
+                    stack.append(curr.next)
+                    curr.next.prev = None
+                curr.next = curr.child
+                curr.child.prev = curr
+                curr.child = None
+                
+            if curr.next:
+                curr = curr.next
+            else:
+                break
+        
+        while stack:
+            curr.next = stack.pop()
+            curr.next.prev = curr
+            while curr and curr.next:
+                curr= curr.next
+            
+            
+        return head
+    
+        
+        
+        
+        
+        
+        
